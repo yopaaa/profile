6c19d41d0b1a904fae952caf2259da1bb287d090
yopa <90709631+yopaaa@users.noreply.github.com>
2023-05-08 19:24:45 +0700

    Update test.js

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -2,7 +2,7 @@ import { readdir } from 'fs'
 import ResponseApi from '../../js/ResponseApi'
 
 export default function handler(req, res) {
-  readdir('./src/js', { encoding: 'utf8' }, (err, files) => {
+  readdir('./', { encoding: 'utf8' }, (err, files) => {
     console.log(err)
     console.log(files)
     ResponseApi(req, res, 200, { name: 'John Doe', data: files })
